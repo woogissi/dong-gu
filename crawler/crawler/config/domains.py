f9cbf7923bf5c8b81caaee55df739575cf482548
jihoon419 <karljh0123@gmail.com>
2026-05-06 18:20:13 +0900

    refactor: 시드 확장

--- crawler/crawler/config/domains.py
+++ crawler/crawler/config/domains.py
@@ -5,6 +5,18 @@ ALLOWED_HOSTS = {           # 정적 탐색에서 광고등등을 막기 위함.
     "ipsi.deu.ac.kr",
     "dorm.deu.ac.kr",
     "lib.deu.ac.kr",
+    "ipp.deu.ac.kr",
+    "career.deu.ac.kr",
+    "lifelong.deu.ac.kr",
+    "ctl.deu.ac.kr",
+    "collabo.deu.ac.kr",
+    "has.deu.ac.kr",
+    "bhcoss.deu.ac.kr",
+    "deuhome.deu.ac.kr",
+    "counsel.deu.ac.kr",
+    "advising.deu.ac.kr",
+    "webzine.deu.ac.kr",
+    "deufund.deu.ac.kr",
 }
 
 DOWNLOAD_EXTENSIONS = {     # 정적 크롤링 탐색에서 첨부파일 다운을 막기위한 예외 주소
